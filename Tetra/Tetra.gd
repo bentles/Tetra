e44b3d54e6c1e20b2e8d6a97fe5d8dae9498fa92
bentles <dee.bent.lee@gmail.com>
2022-03-30 19:43:47 +0200

    trying unsplit. getting messy why is y flipped for screen coords

--- Tetra/Tetra.gd
+++ Tetra/Tetra.gd
@@ -4,6 +4,7 @@ var flipping = false
 var flipped = false
 var split = false
 var children: Array
+var to_middle: Vector2
 var depth = 0
 
 const gap = 0.06
@@ -21,19 +22,30 @@ func _ready():
 			var tetra = Tetra.instance()
 			tetra.scale_revert = scale_revert * (1/scale)
 			tetra.depth = depth + 1
+			tetra.to_middle = Vector2(-i * 0.707106, j * 0.707106)
 			tetra.translate(Vector3(i * (2.5 + gap_offset),j * (2.5 + gap_offset), 0))
 			tetra.scale_object_local(Vector3(scale, scale, scale))
 		
 			children.push_back(tetra)
 	$TetraBody.scale_object_local(Vector3(1, 1, scale_revert))
 
-func flip(reversed: bool):
-	flipping = true
-	if (reversed):
-		$AnimationPlayer.play_backwards("flip" if flipped else "flip2")
+func flip(relative: Vector2):
+	var unsplit = 0
+	if (to_middle != null):
+		unsplit = relative.dot(to_middle)
+	
+	var drag = relative.dot(Vector2.UP)
+	
+	if (unsplit > abs(drag)):
+		unsplit_parent()
 	else:
-		$AnimationPlayer.play("flip2" if flipped else "flip")
-	flipped = !flipped
+		flipping = true
+		if (drag < 0):
+			$AnimationPlayer.play_backwards("flip" if flipped else "flip2")
+		else:
+			$AnimationPlayer.play("flip2" if flipped else "flip")
+		flipped = !flipped
+	
 	
 func split():
 	if depth >= 3:
@@ -43,6 +55,15 @@ func split():
 	$TetraBody.hide()
 	for child in children:
 		add_child(child)
+		
+func unsplit_parent():
+	var a = get_parent().unsplit()
+		
+func unsplit():
+	split = false
+	$TetraBody.show()
+	for child in children:
+		remove_child(child)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -53,7 +74,7 @@ func _on_TetraBody_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventScreenTouch && event.pressed:
 		touch_down = true
 	elif event is InputEventScreenDrag && touch_down && !flipping:
-		flip(event.relative.dot(Vector2.UP) < 0)
+		flip(event.relative)
 	elif event is InputEventScreenTouch && !event.pressed && touch_down && !flipping:
 		touch_down = false
 		split()
