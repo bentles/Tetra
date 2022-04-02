415eeb58bbb44374eb4129b6d6dfc0b1ec61deec
bentles <dee.bent.lee@gmail.com>
2022-04-02 18:06:39 +0200

    added slide forward animation

--- Game.gd
+++ Game.gd
@@ -18,10 +18,12 @@ func create_new_goal():
 	rng.randomize()
 	goal_tetra.translate(Vector3(0, 5.5, 0))
 	goal_tetra.is_interactive = false
-	add_child(goal_tetra)
+	$GoalTetra.add_child(goal_tetra)
 	goal_tetra.random_operation(5, rng)
+	$GoalTetra/GoalAnimation.play("slide_forwards")
 
-func _on_Timer_timeout():
+
+func _on_GoalAnimation_animation_finished(anim_name):
 	goal_tetra.queue_free()
 	create_new_goal()
-	pass # Replace with function body.
+	 # Replace with function body.
