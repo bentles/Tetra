7ea16fb1b15688e52cf2ea85721fe5a2ef8b36db
bentles <dee.bent.lee@gmail.com>
2022-04-02 17:12:30 +0200

    updating

--- Game.gd
+++ Game.gd
@@ -1,23 +1,27 @@
 extends Spatial
 
+var difficulty = 1
+const Tetra = preload("res://Tetra/Tetra.tscn")
+var goal_tetra
 
 func _ready():
-	var test = [false, [false, true, true, false], 
-				[false, true, true, [false, true, true, false]], 
-				[false, false, false, false]]
-	
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	$Tetra.random_operation(2, rng)
-	
+	create_new_goal()
 	pass
-
-
-func _on_Tetra_change():
-	
-	pass # Replace with function body.
-
 
 func _on_Tetra2_change():
 	print($Tetra2.serialize())
+	pass # Replace with function body.
+
+func create_new_goal():
+	goal_tetra = Tetra.instance()
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	goal_tetra.translate(Vector3(0, 5.5, 0))
+	goal_tetra.is_interactive = false
+	add_child(goal_tetra)
+	goal_tetra.random_operation(5, rng)
+
+func _on_Timer_timeout():
+	goal_tetra.queue_free()
+	create_new_goal()
 	pass # Replace with function body.
--- Tetra/Tetra.gd
+++ Tetra/Tetra.gd
@@ -1,3 +1,4 @@
+class_name Tetra
 extends Spatial
 
 signal change
