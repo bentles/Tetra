eebb42537ee521df99692b2e6dcb237745f23883
bentles <dee.bent.lee@gmail.com>
2022-04-15 21:11:26 +0200

    added domino flip :O

--- Tetra/Tetra.gd
+++ Tetra/Tetra.gd
@@ -7,8 +7,11 @@ var flipping := false
 var flipped := false
 var is_split := false
 var children := []
-var to_middle: Vector2
 var depth = 0
+
+const UPPER_BOUND = 8
+var x_bounds = [0, UPPER_BOUND]
+var y_bounds = [0, UPPER_BOUND]
 
 export var is_interactive: bool = true
 
@@ -29,14 +32,14 @@ func _ready():
 func _create_children():
 	var gap_offset = gap * pow(2, depth)
 	var scale = (5 - (gap_offset * 2)) / 10
-	for i in [-1, 1]:
-		for j in [-1, 1]:
+	for y in [-1, 1]:
+		for x in [-1, 1]:
 			var tetra = Tetra.instance()
 			tetra.scale_revert = scale_revert * (1/scale)
 			tetra.is_interactive = is_interactive
 			tetra.depth = depth + 1
-			tetra.to_middle = Vector2(-i, -j)
-			tetra.translate(Vector3(i * (2.5 + gap_offset),j * (2.5 + gap_offset), 0))
+			tetra.calculate_bounds(x,y, x_bounds, y_bounds)
+			tetra.translate(Vector3(x * (2.5 + gap_offset), y * (2.5 + gap_offset), 0))
 			tetra.scale_object_local(Vector3(scale, scale, scale))
 			children.push_back(tetra)
 
@@ -58,6 +61,8 @@ func flatten() -> Array:
 		for child in children:
 			all += child.flatten()
 		return all
+		
+
 
 func random_operation(n: int, rng: RandomNumberGenerator):
 	if n == 0:
@@ -99,9 +104,58 @@ func bubble_change():
 	else:
 		get_parent().bubble_change()
 
-func flip(relative: Vector2):
-	var dragY = relative.dot(Vector2.UP)
-	var dragX = relative.dot(Vector2.LEFT)
+func calculate_bounds(x,y, x_bds, y_bds):
+	var change = UPPER_BOUND >> depth # :)
+	x_bounds = _calc_bound(change, x, x_bds)
+	y_bounds = _calc_bound(change, y, y_bds)
+	pass
+	
+func _calc_bound(change, direction, bound):
+	var bound_copy = bound.duplicate()
+	if direction > 0:
+		bound_copy[0] += change
+	else:
+		bound_copy[1] -= change
+	return bound_copy
+		
+func should_domino(x_bds, y_bds, direction: Vector2):
+	if is_split:
+		for child in children:
+			child.should_domino(x_bds, y_bds, direction)
+	else:
+		var in_range = false
+		if (abs(direction.x) > abs(direction.y)):
+			in_range = _check_ranges(y_bds, y_bounds, x_bds, x_bounds, direction.x > 0)
+		else:
+			in_range = _check_ranges(x_bds, x_bounds, y_bds, y_bounds, direction.y < 0)
+		if in_range:
+			domino_flip(direction)
+	pass
+	
+func _check_ranges(is_align, with_align, is_adjacent, with_adjacent, forwards: bool):
+	var aligned = is_align[0] < with_align[1] && with_align[0] < is_align[1]
+		
+	var adjacent = false
+	if forwards:
+		adjacent = is_adjacent[1] == with_adjacent[0]
+	else:
+		adjacent = is_adjacent[0] == with_adjacent[1]
+		
+	return aligned && adjacent
+
+func domino_flip(direction: Vector2):
+		flip(direction)
+		
+		var tetra = self
+		while tetra.depth > 0:
+			tetra = tetra.get_parent()
+			
+		tetra.should_domino(x_bounds, y_bounds, direction)
+		pass
+
+func flip(direction: Vector2):
+	var dragY = direction.dot(Vector2.UP)
+	var dragX = direction.dot(Vector2.LEFT)
 	
 	var vertical = abs(dragY) > abs(dragX)
 	var drag = dragY if vertical else dragX
@@ -166,13 +220,13 @@ func handle_event(event: GameInputEvent):
 	if (event.event_type == GameInputEvent.SWIPE):
 		flip(event.end_pos - event.pressed_pos)
 	if (event.event_type == GameInputEvent.FAST_SWIPE):
-		flip(event.end_pos - event.pressed_pos)
+		domino_flip(event.end_pos - event.pressed_pos)
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if !is_interactive:
 		return
 		
-	if (anim_name == "flip" || anim_name == "flip2"):
+	if "flip" in anim_name:
 		flipping = false
 	pass # Replace with function body.
 
